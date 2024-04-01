bd1a36d5fd12267b40e69f3ee94e9ff9d594255c
Francisco.V98 <frncsico@gmail.com>
2024-04-01 15:47:47 -0400

    create background color dinamic with palette generator

--- lib/widgets/card_pokemon_list_view.dart
+++ lib/widgets/card_pokemon_list_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 import 'package:pokedex_app/widgets/widgets.dart';
 
-class CardPokemonListView extends StatelessWidget {
+class CardPokemonListView extends StatefulWidget {
   final Color backgroundColor;
   final int idPokemon;
   final int imgPokemon;
@@ -16,20 +17,51 @@ class CardPokemonListView extends StatelessWidget {
   });
 
   @override
+  State<CardPokemonListView> createState() => _CardPokemonListViewState();
+}
+
+class _CardPokemonListViewState extends State<CardPokemonListView> {
+  late Color _backgroundColor = Colors.blueAccent;
+
+  @override
+  void initState() {
+    super.initState();
+    _generatePalette();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> _generatePalette() async {
+    final paletteGenerator = await PaletteGenerator.fromImageProvider(
+      NetworkImage(
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${widget.imgPokemon}.png'),
+    );
+
+    if (mounted) {
+      setState(() {
+        _backgroundColor = paletteGenerator.dominantColor!.color;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {},
       child: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: backgroundColor,
+          color: _backgroundColor,
           borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              namePokemon,
+              widget.namePokemon,
               style: const TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w500,
@@ -38,7 +70,7 @@ class CardPokemonListView extends StatelessWidget {
               ),
             ),
             Text(
-              '#${idPokemon.toString().padLeft(3, '0')}',
+              '#${widget.idPokemon.toString().padLeft(3, '0')}',
               style: const TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w300,
@@ -49,7 +81,7 @@ class CardPokemonListView extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(bottom: 4),
               child: Image.network(
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$imgPokemon.png',
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${widget.imgPokemon}.png',
                 // width: 50,
                 height: 70,
                 fit: BoxFit.contain,
